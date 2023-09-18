e33fd1c4541edb9861347239429b9cc79975963b
rezamohdev <mohammadireza29@gmail.com>
2023-09-18 08:01:42 -0400

    leetcode

--- 27. Remove Element/solution.js
+++ 27. Remove Element/solution.js
@@ -1,10 +1,10 @@
-**
+/**
  * @param {number[]} nums
  * @param {number} val
  * @return {number}
  */
 // new code 10.0.0.197:808
-	var removeElement = function (nums, val) {
+var removeElement = function (nums, val) {
     if (nums.length == 0)
         return 0
     let firstPoint = 0;
@@ -14,5 +14,5 @@
 
 
     return firstPoint;
-:w
-	};
+
+};

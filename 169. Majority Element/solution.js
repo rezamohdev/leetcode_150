ba0cfc284720d6ac4094bf05a10e01be821267ae
rezamohdev <mohammadireza29@gmail.com>
2023-10-24 21:05:58 -0400

    169. Majority Element

---
+++ 169. Majority Element/solution.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+    let empty = [];
+    nums.sort((a, b) => a - b);
+    let maxCount = 0;
+    let index = -1;
+
+    for (let i = 0; i <= nums.length - 1; i++) {
+        let count = 0;
+        for (let j = 0; j <= nums.length - 1; j++) {
+            if (nums[i] == nums[j]) {
+                count++;
+            }
+            if (count > maxCount) {
+                maxCount = count;
+                index = i;
+            }
+        }
+    }
+
+    if (maxCount > nums.length / 2) {
+        return nums[index];
+    }
+};

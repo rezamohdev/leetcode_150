697b5eefdb46c0d4ba1fbdf3cb7622cf447b37e7
rezamohdev <mohammadireza29@gmail.com>
2023-10-28 20:57:10 -0400

    function

--- 13. Roman to Integer/solution.js
+++ 13. Roman to Integer/solution.js
@@ -1,21 +1,23 @@
-const o = {
-    I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000
-};
+var romanToInt = function (s) {
+    const o = {
+        I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000
+    };
 
-let rv = 0;
-let rn0;
-let rn1;
+    let rv = 0;
+    let rn0;
+    let rn1;
 
-for (let i = s.length - 1; i >= 0; i--) {
+    for (let i = s.length - 1; i >= 0; i--) {
 
-    rn0 = o[s[i]];
-    rn1 = o[s[i - 1]];
+        rn0 = o[s[i]];
+        rn1 = o[s[i - 1]];
 
-    if (rn1 < rn0) {
-        rv += (rn0 - rn1);
-        i--;
-    } else {
-        rv += rn0;
+        if (rn1 < rn0) {
+            rv += (rn0 - rn1);
+            i--;
+        } else {
+            rv += rn0;
+        }
     }
+    return rv;
 }
-return rv;

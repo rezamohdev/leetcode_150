80748de3e77aecf3b2ad5117348f33fcef087092
rezamohdev <mohammadireza29@gmail.com>
2023-11-04 22:21:05 -0400

    26. Remove Duplicates from Sorted Array 2

---
+++ 26. Remove Duplicates from Sorted Array/s2.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    var len = nums.length;
+    var last = NaN;
+    var count = 0;
+    for (var i = 0; i < len; i++) {
+        if (nums[i] !== last) {
+            nums[count] = nums[i];
+            last = nums[i];
+            count++;
+        }
+    }
+    return count;
+};

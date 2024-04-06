ea73978c15908fddca57d66ce920bf279e0b1784
Reza Mohammadi <56708670+rezamohdev@users.noreply.github.com>
2024-04-05 22:15:55 -0400

    sortbitbyones

---
+++ 1356. Sort Integers by The Number of 1 Bits/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {number[]}
+ */
+var sortByBits = function(arr) {
+    return arr.sort((a, b) => bitCount(a) - bitCount(b) || a - b); 
+ };
+ 
+ function bitCount(num){
+     let sum = 0;
+     while(num){
+         sum += num & 1;
+         num = num >> 1;
+     }
+     return sum;
+ }
